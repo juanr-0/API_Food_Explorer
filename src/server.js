b879cecf91db4e132dbf71fd1a4ecc8bda41ade5
juan rodrigues <eng.juan.rodrigues@gmail.com>
2023-11-02 18:27:27 -0300

    cors atualiza

--- src/server.js
+++ src/server.js
@@ -6,40 +6,48 @@ const express = require("express");
 const routes = require("./routes");
 const database = require("./database/sqlite");
 const uploadConfig = require("./configs/upload");
-
-
 const cookieParser = require("cookie-parser");
 const AppError = require("./utils/AppError");
 
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({
-  origin: ["http://localhost:5173","http://127.0.0.1:5173/"],
-  credentials: true
-}));
+
+// Configuração CORS para permitir solicitações da origem Netlify
+const allowedOrigins = ["https://quiet-zuccutto-0eff04.netlify.app"];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    credentials: true,
+  })
+);
 
 app.use("/files", express.static(uploadConfig.UPLOADS_FOLDER));
-
 app.use(routes);
 database();
 
 app.use((err, request, response, next) => {
-    if (err instanceof AppError) {
-      return response.status(err.statusCode).json({
-        status: "error",
-        message: err.message,
-      });
-    }
-  
-    console.error(err);
-  
-    return response.status(500).json({
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
       status: "error",
-      message: "Internal server error",
+      message: err.message,
     });
-});
+  }
 
+  console.error(err);
+
+  return response.status(500).json({
+    status: "error",
+    message: "Internal server error",
+  });
+});
 
 const PORT = process.env.PORT || 3333;
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`));
